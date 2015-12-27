fbf29ae2395363099f371e41796cf088993eb722
jkasztur <jan.kasztura@seznam.cz>
2015-12-27 18:12:17 +0100

    Homework 9 - mmap, fault

--- ulohy/09/pb173.c
+++ ulohy/09/pb173.c
@@ -4,61 +4,135 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/mm.h>
+#include <linux/kernel.h>
+#include <asm/uaccess.h>
+#include <linux/mman.h>
 void *temp1;
 void *temp2;
 void *temp3;
 void *temp4;
 
+char *buffer;
+int task_mapped;
+
+unsigned int fun(unsigned int data);
+
+int my_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
+{
+	struct page *page = 0;
+	unsigned long offset;
+
+	pr_info("myfault beginning\n");
+	offset = vmf->pgoff;
+	pr_info("offset: %lx", offset);
+	if ((0 <= offset) && (offset < 1)) {
+		if ((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ) {
+			pr_info("Bad flags\n");
+			return -EINVAL;
+		}
+		pr_info("returning temp1\n");
+		page = vmalloc_to_page(temp1);
+		if (!page) {
+			pr_warn("page is not set, offset is %lx\n", offset);
+			return VM_FAULT_SIGBUS;
+		}
+	}
+	if ((1 <= offset) && (offset < 2)) {
+		if ((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ) {
+			pr_info("Bad flags\n");
+			return -EINVAL;
+		}
+		pr_info("returnnig temp2\n");
+		page = vmalloc_to_page(temp2);
+		if (!page) {
+			pr_warn("page is not set, offset is %lx\n", offset);
+			return VM_FAULT_SIGBUS;
+		}
+	}
+	if ((2 <= offset) && (offset < 3)) {
+		if ((vma->vm_flags & (VM_READ | VM_WRITE))
+			!= (VM_READ | VM_WRITE)) {
+			pr_info("Bad flags\n");
+			return -EINVAL;
+		}
+		pr_info("returning temp3\n");
+		page = virt_to_page(temp3);
+	}
+	if ((3 <= offset) && (offset < 4)) {
+		if ((vma->vm_flags & (VM_READ | VM_WRITE))
+			!= (VM_READ | VM_WRITE)) {
+			pr_info("Bad flags\n");
+			return -EINVAL;
+		}
+		pr_info("returning temp4\n");
+		page = virt_to_page(temp4);
+	}
+	if (!page) {
+		pr_warn("page is not set, offset is %lx\n", offset);
+		return VM_FAULT_SIGBUS;
+	}
+	get_page(page);
+	vmf->page = page;
+	return 0;
+}
+
+struct vm_operations_struct my_vm_ops = {
+	.fault = my_fault,
+};
+
 int my_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	pr_info("my_mmap starting");
-	if((0 <= vma->vm_pgoff) && (vma->vm_pgoff < 1))
-	{
-		if((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ)
-		{
-			pr_info("Bad flags, my comment");
+	unsigned int return_value;
+	unsigned int (*fun)(unsigned int);
+	pr_info("my_mmap starting\n");
+	if (task_mapped == 0) {
+		remap_vmalloc_range_partial(vma, 0x800000, buffer, 10000);
+		task_mapped = 1;
+		pr_info("task.bin has been successfuly mapped\n");
+		fun = (void *)0x800000;
+		return_value = (*fun)(0);
+		pr_info("function 0: %u\n", return_value);
+	/*	pr_info("function 1: %u\n", (*fun)(1));
+		pr_info("function 45: %u\n", (*fun)(45));
+		pr_info("function 90: %u\n", (*fun)(90));
+	*/
+	}
+	if ((0 <= vma->vm_pgoff) && (vma->vm_pgoff < 1)) {
+		if ((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ) {
+			pr_info("Bad flags1\n");
 			return -EINVAL;
 		}
-		return remap_vmalloc_range(vma, temp1, 0);
+		vma->vm_ops = &my_vm_ops;
+		return 0;
 	}
-	if((1 <= vma->vm_pgoff) && (vma->vm_pgoff < 2))
-	{
-		if((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ)
-		{
-			pr_info("Bad flags, my comment");
+	if ((1 <= vma->vm_pgoff) && (vma->vm_pgoff < 2)) {
+		if ((vma->vm_flags & (VM_WRITE | VM_READ)) != VM_READ) {
+			pr_info("Bad flags2\n");
 			return -EINVAL;
 		}
-		return remap_vmalloc_range(vma, temp2, 0);
+		vma->vm_ops = &my_vm_ops;
+		return 0;
 	}
-	if((2 <= vma->vm_pgoff) && (vma->vm_pgoff < 3))
-	{
-		if((vma->vm_flags & (VM_READ | VM_WRITE)) != (VM_READ | VM_WRITE))
-		{
-			pr_info("not read and write, my_com");
-			return -EINVAL;		
+	if ((2 <= vma->vm_pgoff) && (vma->vm_pgoff < 3)) {
+		if ((vma->vm_flags & (VM_READ | VM_WRITE))
+			!= (VM_READ | VM_WRITE)) {
+			pr_info("Bad flags3\n");
+			return -EINVAL;
 		}
-		return remap_pfn_range(vma, vma->vm_start, page_to_pfn(virt_to_page(temp3)),
-			PAGE_SIZE, vma->vm_page_prot);
+		vma->vm_ops = &my_vm_ops;
+		return 0;
 	}
-	if((3 <= vma->vm_pgoff) && (vma->vm_pgoff < 4))
-	{
-		if((vma->vm_flags & (VM_READ | VM_WRITE)) != (VM_READ | VM_WRITE))
-		{
-			pr_info("not read and write, my_com");
-			return -EINVAL;		
+	if ((3 <= vma->vm_pgoff) && (vma->vm_pgoff < 4)) {
+		if ((vma->vm_flags & (VM_READ | VM_WRITE))
+			!= (VM_READ | VM_WRITE)) {
+			pr_info("Bad flags4\n");
+			return -EINVAL;
 		}
-		return remap_pfn_range(vma, vma->vm_start + PAGE_SIZE,
-			page_to_pfn(virt_to_page(temp4)),
-			PAGE_SIZE, vma->vm_page_prot);
+		vma->vm_ops = &my_vm_ops;
+		return 0;
 	}
-	else
-	{
-		pr_info("out of range, mycom");
-		return -EFAULT;
-
-	}
-	pr_info("mymap finished successfully");
-	return 0;
+	pr_info("out of range\n");
+	return -EFAULT;
 }
 
 const struct file_operations hw9_fops = {
@@ -72,31 +146,53 @@ struct miscdevice my_device = {
 	.fops = &hw9_fops,
 };
 
+struct file *f;
+
 static int my_init(void)
 {
-	pr_info("Adding module.");
+	mm_segment_t fs;
+
+	pr_info("Adding module.\n");
+	task_mapped = 0;
 	temp1 = vmalloc_user(PAGE_SIZE);
 	temp2 = vmalloc_user(PAGE_SIZE);
-	temp3 = (void*)__get_free_page(GFP_KERNEL);
-	temp4 = (void*)__get_free_page(GFP_KERNEL);
+	temp3 = (void *)__get_free_page(GFP_KERNEL);
+	temp4 = (void *)__get_free_page(GFP_KERNEL);
 	strcpy(temp1, "aaa");
 	strcpy(temp2, "bbb");
 	strcpy(temp3, "ccc");
 	strcpy(temp4, "ddd");
-	
-	misc_register(&my_device);	
+	pr_info("temp1: %p\n", temp1);
+	pr_info("temp2: %p\n", temp2);
+	pr_info("temp3: %p\n", temp3);
+	pr_info("temp4: %p\n", temp4);
+
+	buffer = vmalloc(10000);
+	memset(buffer, 0, 10000);
+	f = filp_open("/home/vyvoj/repos/pb173/09/task.bin", O_RDONLY, 0);
+	if (f == NULL) {
+		pr_info("filp_open error!\n");
+		return -EIO;
+	}
+	fs = get_fs();
+	set_fs(get_ds());
+	f->f_op->read(f, buffer, 10000, &f->f_pos);
+	set_fs(fs);
+	pr_info("Loading task.bin to buffer finished\n");
+	filp_close(f, NULL);
+	misc_register(&my_device);
 	return 0;
 }
 
 static void my_exit(void)
 {
+	vfree(buffer);
 	vfree(temp1);
 	vfree(temp2);
 	free_page((unsigned long)temp3);
 	free_page((unsigned long)temp4);
-	
 	misc_deregister(&my_device);
-	pr_info("Removing module.");
+	pr_info("Removing module.\n");
 }
 
 module_init(my_init);
